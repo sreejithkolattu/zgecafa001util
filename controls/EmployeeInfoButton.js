515fdcf4866c107d40a5306bde0a252abb568d90
S0019614292 <build.demo.06@gmail.com>
2019-05-14 10:46:57 +0000

    Import content for version 1.0.4191

---
+++ controls/EmployeeInfoButton.js
@@ -0,0 +1,4 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define(["jquery.sap.global","./../library","sap/m/Button"],function(e,t,o){"use strict";var r=o.extend("zgecafa001util.zgecafa001util.controls.EmployeeInfoButton",{metadata:{events:{employeeHover:{}}},onmouseover:function(e){this.fireEmployeeHover()},renderer:{}});return r},true);
