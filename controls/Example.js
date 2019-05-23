2d4b1e033fb6a41801f9a8eecfd7de94144a0e33
S0019614292 <build.demo.06@gmail.com>
2019-05-23 15:25:36 +0000

    Import content for version 1.0.4204

---
+++ controls/Example.js
@@ -0,0 +1,4 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define(["jquery.sap.global","./../library","sap/ui/core/Control"],function(e,a,t){"use strict";var r=t.extend("zgecafa001util.zgecafa001util.controls.Example",{metadata:{library:"zgecafa001util.zgecafa001util",properties:{text:{type:"string",group:"Misc",defaultValue:null}},events:{press:{}}}});return r},true);
