896063b284c50c11c75912c9d5a40f6e5536ba1c
S0019614292 <build.demo.06@gmail.com>
2019-08-21 09:59:43 +0000

    Import content for version 1.0.4286

---
+++ library.js
@@ -0,0 +1,4 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define(["jquery.sap.global","sap/ui/core/library"],function(){"use strict";sap.ui.getCore().initLibrary({name:"zgecafa001util.zgecafa001util",version:"1.0.0",dependencies:["sap.ui.core"],types:[],interfaces:[],controls:["zgecafa001util.zgecafa001util.controls.Example"],elements:[]});return zgecafa001util.zgecafa001util},false);
