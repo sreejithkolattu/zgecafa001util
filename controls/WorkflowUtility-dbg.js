65f951d24829c436060e11616b795c51b82bef90
S0019614292 <build.demo.06@gmail.com>
2019-08-29 06:51:29 +0000

    Import content for version 1.0.4354

---
+++ controls/WorkflowUtility-dbg.js
@@ -0,0 +1,106 @@
+sap.ui.define(["jquery.sap.global", "sap/ui/core/Control"], function (jQuery, Control) {
+	"use strict";
+	/**
+	 * @author Sreejith Ravindran
+	 * @version ${1.0}
+	 * Date: Nov 3, 2018
+	 */
+	return {
+		/**
+		 * Function showSignatureFragment (fragInst,fragmentId) 
+		 * @param {fragInst} Current Instance 
+		 * @param {fragmentId} Id of fragment 
+		 * @return {} 
+		 * @public
+		 */
+
+		handleLocking: function (currInst, wid) {
+			var that = currInst;
+			/*var serviceUrl1 = "/sap/opu/odata/sap/Y88GECAGS004_SRV/";
+			var sPath = "/Check_Wi_Lock?Wid=" + wid;*/
+			var serviceUrl1 = "/sap/opu/odata/sap/ZGECAGS010_SRV/";
+			var sPath = "/MultiplePersonaSet";
+			var oDataModel = new sap.ui.model.odata.v2.ODataModel(serviceUrl1);
+			oDataModel.read(sPath, {
+				success: function (oData, response) {
+					sap.m.MessageToast.show("Success..!!");
+					//Browser close functionalities
+					if ("addEventListener" in window) {
+						window.addEventListener("beforeunload", onLeaveApp);
+					} else if ("attachEvent" in window) {
+						window.attachEvent("onbeforeunload", onLeaveApp);
+					}
+					if (sap.ui.getCore().byId("backBtn") !== undefined)
+						sap.ui.getCore().byId("backBtn").attachBrowserEvent("click", onLeaveApp, this);
+					if (sap.ui.getCore().byId("homeBtn") !== undefined)
+						sap.ui.getCore().byId("homeBtn").attachBrowserEvent("click", onLeaveApp, this);
+
+				},
+				error: function (err) {
+					sap.m.MessageToast.show("Error..!!");
+				}
+
+			});
+
+		},
+		handleUnLocking: function (currInst, wid) {
+			var that = currInst;
+			/* oDataModel.callFunction("/Unlock_Wid", {
+                        method: "POST",
+                        urlParameters: {
+                            Wid: this.workItemId
+                        },
+                        success: function (msg, oResponse) {
+                            console.log("success" + oResponse);
+                        },
+                        error: function (oError) {
+                            return;
+                        }
+			*/
+			var serviceUrl1 = "/sap/opu/odata/sap/ZGECAGS010_SRV/";
+			var sPath = "/MultiplePersonaSet";
+			var oDataModel = new sap.ui.model.odata.v2.ODataModel(serviceUrl1);
+			oDataModel.read(sPath, {
+				success: function (oData, response) {
+					sap.m.MessageToast.show("Success..!!");
+					//Browser close functionalities
+
+					if ("removeEventListener" in window) {
+						window.removeEventListener("beforeunload", onLeaveApp);
+					} else if ("detachEvent" in window) {
+						window.detachEvent("onbeforeunload", onLeaveApp);
+					}
+					if (sap.ui.getCore().byId("backBtn") !== undefined)
+						sap.ui.getCore().byId("backBtn").detachBrowserEvent("click", onLeaveApp, this);
+					if (sap.ui.getCore().byId("homeBtn") !== undefined)
+						sap.ui.getCore().byId("homeBtn").detachBrowserEvent("click", onLeaveApp, this);
+				},
+				error: function (err) {
+					sap.m.MessageToast.show("Error..!!");
+				}
+
+			});
+
+		}
+
+	};
+
+	function onLeaveApp() {
+		var oDialog = new sap.m.Dialog({
+			icon: "sap-icon://SAP-icons-TNT/exceptions",
+			title: "Workitem Locked",
+			content: [new sap.m.Text({
+				text: "Please unlock your workitem"
+			})],
+			endButton: new sap.m.Button({
+				text: "OK",
+				press: function () {
+					oDialog.close();
+				}
+			})
+		});
+		oDialog.open();
+		return false;
+	}
+
+}, /* bExport= */ true);
